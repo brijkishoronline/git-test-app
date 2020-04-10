bd1d0eaa6fa4f184d3f9225b81e3d45bdddd067f
Brij Yadav <F5082080@FNB.CO.ZA>
2020-04-10 18:05:00 +0200

    changes for new DTO

---
+++ src/main/java/com/javatechie/spring/api/Order.java
@@ -0,0 +1,35 @@
+package com.javatechie.spring.api;
+
+public class Order {
+	
+	private String userName;
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getAmount() {
+		return amount;
+	}
+
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+
+	private String productName;
+	
+	private String amount;
+
+}
